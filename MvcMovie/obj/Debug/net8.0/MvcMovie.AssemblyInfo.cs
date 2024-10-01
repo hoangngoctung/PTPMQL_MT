a72b940dd8d15c3f3993dc0ec70f201ba9e2ee1a
hoangngoctung <hoangngoctung22@gmail.com>
2024-10-01 09:37:35 +0700

    CreatProject

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a98192eabf9e844914a629c0e65b7569c5ab45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584d2c00b997e3fd189264eed2b28d4607f47a36")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
